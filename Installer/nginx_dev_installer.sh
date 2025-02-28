217171906739f836061ef41e276e0962cfd82e34
René Kreijveld <email@renekreijveld.nl>
2025-02-28 20:20:00 +0100

    Fixed bug adminer installer

--- Installer/nginx_dev_installer.sh
+++ Installer/nginx_dev_installer.sh
@@ -168,7 +168,7 @@ install_joomla_scripts() {
 
 install_root_tools() {
     cd "${SITESROOT}"
-    echo "Downloading Adminer script
+    echo "Installing adminer.php script"
     curl -L "https://www.adminer.org/latest.php" > adminer.php
     echo "<?php phpinfo();" > phpinfo.php
 }
